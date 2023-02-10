b2b4af7e1be78900867ebebdf9d6ec1347a3f117
Inozcente Rodriguez <BOOKINHAND@GMAIL.COM>
2023-02-10 06:31:45 -0300

    add tests
    Add tests

---
+++ Sat.Recruitment.Test/UserIncomplete.cs
@@ -0,0 +1,23 @@
+﻿using Sat.Recruitment.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sat.Recruitment.Test
+{
+    public class UserIncomplete
+    { 
+         
+          
+            public string Email { get; set; }
+          
+            public string Address { get; set; }
+          
+            public string Phone { get; set; }
+          
+            public String UserType { get; set; }
+           
+        
+    }
+}
